cf9efdc7301a9d9fb69e55462095f355bd3bbb46
jhlee <wisdom_13_@naver.com>
2024-02-01 02:42:26 +0900

    module

---
+++ 1-node/4-module/app.js
@@ -0,0 +1,5 @@
+const counter = require('./counter');
+
+counter.increase();
+counter.increase();
+console.log(counter.getCount());
---
+++ 1-node/5-module/app.js
@@ -0,0 +1,6 @@
+import * as counter from './counter.js';
+// import { increase, getCount } from './counter.js';
+
+counter.increase();
+counter.increase();
+console.log(counter.getCount());
