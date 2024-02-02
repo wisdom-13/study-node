cb5bc5a52831b5895ef3de3a56f3d4324eda6b8d
jhlee <wisdom_13_@naver.com>
2024-02-02 19:12:27 +0900

    process

---
+++ 1-node/7-process/app.js
@@ -0,0 +1,19 @@
+const process = require('process');
+
+console.log(process.execPath);
+console.log(process.version);
+console.log(process.pid);
+console.log(process.ppid);
+console.log(process.platform);
+console.log(process.env);
+console.log(process.uptime());
+console.log(process.cwd());
+console.log(process.cpuUsage());
+
+setTimeout(() => {
+  console.log('setTimeout')
+}, 0)
+
+process.nextTick(() => {
+  console.log('nextTick');
+})
