9360a87e8455be7cd140706bddf61174e67f5857
jhlee <wisdom_13_@naver.com>
2024-02-16 01:18:05 +0900

    미들웨어

--- 6-express/app.js
+++ 6-express/app.js
@@ -2,16 +2,53 @@ import express from 'express';
 
 const app = express();
 
+// request와 response
 app.get('/sky/:id', (req, res, next) => {
   console.log(req.params);
   console.log(req.params.id);
   console.log(req.query);
   console.log(req.query.keyword);
 
-
   res.setHeader('key', 'value');
   // res.json({ name: 'jihye' });
   res.status(201).send('created');
+})
+
+// all과 use의 차이점
+app.all('/api/*', (req, res, next) => {
+  console.log('all');
+  next();
+})
+
+app.use('/api2', (req, res, next) => {
+  console.log('use');
+  next();
+})
+
+
+// 미들웨어의 특징
+app.get('/', (req, res, next) => {
+  console.log('first');
+  if (true) {
+    return res.send('jihye')
+  }
+  res.send('Hi')
+}, (req, res, next) => {
+  console.log('first2');
+  next(new Error('error'));
+})
+
+app.get('/', (req, res, next) => {
+  console.log('second');
+})
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
+})
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send('Error')
 })
 
 app.listen(8080);
