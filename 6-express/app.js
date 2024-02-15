066835a6c02e2f36aaf15caf302f5092921ec2b7
jhlee <wisdom_13_@naver.com>
2024-02-15 22:18:30 +0900

    request

--- 6-express/app.js
+++ 6-express/app.js
@@ -2,8 +2,13 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  console.log('get');
+app.get('/sky/:id', (req, res, next) => {
+  console.log(req.params);
+  console.log(req.params.id);
+  console.log(req.query);
+  console.log(req.query.keyword);
+
+
   res.send('hi');
 })
 
