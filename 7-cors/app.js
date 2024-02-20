7270c659ae9bbad39d0c35ade83fa798f03d0d0b
jhlee <wisdom_13_@naver.com>
2024-02-21 01:27:28 +0900

    외부 미들웨어

--- 7-cors/app.js
+++ 7-cors/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
 
 const app = express();
 
@@ -12,15 +16,22 @@ const app = express();
 //   next();
 // })
 
-app.use(
-  cors({
-    origin: ['http://127.0.0.1:5500'],
-    optionsSuccessStatus: 200,
-    credentials: true, // Access-Control-Allow-Credentials: true
-  })
-);
+const corsOption = {
+  origin: ['http://127.0.0.1:5500'],
+  optionsSuccessStatus: 200,
+  credentials: true, // Access-Control-Allow-Credentials: true
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('combined')); // 로그 출력
+app.use(cors(corsOption)); // CORS 설정
+app.use(helmet()); // 보안 관련 header 
 
 app.get('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  console.log(req.cookies.yummy_cookie);
   res.send('Welcom!');
 })
 
