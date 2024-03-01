210e60bfe1f328958ad09e5712f27cc890572270
jhlee <wisdom_13_@naver.com>
2024-03-01 23:53:18 +0900

    validation 리팩토링 - 에러 코드 분리

--- 8-vaildation/app.js
+++ 8-vaildation/app.js
@@ -4,21 +4,28 @@ import { body, param, validationResult } from 'express-validator';
 const app = express();
 app.use(express.json());
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg })
+}
+
 app.post('/users',
   body('name').isLength({ min: 2 }).withMessage('2자 이상 입력하세요.'),
   body('age').isInt().withMessage('숫자를 입력해주세요.'),
   body('email').isEmail().withMessage('이메일을 입력해주세요.'),
+  validate,
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() })
-    }
+
     console.log(req.body);
     res.sendStatus(201);
   })
 
 app.get('/:email',
   param('email').isEmail().withMessage('이메일을 입력해주세요.'),
+  validate,
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
